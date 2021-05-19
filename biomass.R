a615bbfe8542005daf53572395ecd17fdfed5d74
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2021-05-18 17:38:23 -0700

    initial commit

---
+++ biomass.R
@@ -0,0 +1,18 @@
+# Task 1: replicate design-based total biomass per stratum that's in the SQL table
+
+
+# Data and script locations -----------------------------------------------
+# CPUE table to replicate
+# SQL Developer: AFSC--> Other Users --> AI --> Tables --> CPUE
+
+# SQL script to replicate
+# G Drive: G:/GOA/biomass_sizecomp_scripts/biomass/biomass.sql
+
+
+# Stratum areas to use
+# SQL Developer: AFSC--> Other Users --> GOA --> Tables --> GOA_STRATA
+
+
+# Step 1: Get data from Oracle --------------------------------------------
+
+
