a0cba27eac58af20a43e1209f0012ab7dcab661e
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2021-05-24 18:02:36 -0700

    convert into function get_haul_cpue()

--- biomass.R
+++ biomass.R
@@ -38,29 +38,20 @@ for (i in 1:length(a)) {
 
 
 # Get stratum areas from GOA_STRATA table ---------------------------------
-
-
-goa_strata <- read.csv(here::here("..","Oracle connection","data",
-                                  "oracle","goa","goa_strata.csv"))
-ai_strata <- goa_strata %>% 
+goa_strata <- read.csv(here::here(
+  "..", "Oracle connection", "data",
+  "oracle", "goa", "goa_strata.csv"
+))
+ai_strata <- goa_strata %>%
   janitor::clean_names() %>%
-  filter(survey=="AI")
+  filter(survey == "AI")
 
 # Add area swept to each haul ---------------------------------------------
 head(haul)
 nrow(haul)
 haul <- haul %>%
   mutate(AreaSwept_km2 = distance_fished * (0.001 * net_width)) # bc distance in km and width in m
-
-
-# Put datasets together ---------------------------------------------------
-# dat <- catch %>%
-#   left_join(haul,
-#     by = c("hauljoin", "cruisejoin", 
-#            "region", "cruise", "vessel")
-#   ) %>%
-#   mutate(year = floor(cruise / 100))
-
+# This should match the EFFORT column in RACEBASE-->AI-->CPUE
 
 # Get cruise info from RACE.DATA ------------------------------------------
 # AFAIK you can only get the name of the survey from the cruises.csv file, which is from RACEDATA
@@ -74,67 +65,91 @@ cruises <- janitor::clean_names(cruises)
 cruisedat <- cruises %>%
   select(year, survey_name, region, cruisejoin, cruise_data_in_racebase, vessel_country, survey_definition_id)
 
-speciescode <- 30060 # test species: POP
-survey_area <- "AI" # called region in RACEBASE
-survey_yr <- 2018 # survey year
 
-sp_catch <- catch %>%
-  filter(species_code == speciescode)
+get_haul_cpue <- function(speciescode = 30060, survey_area = "AI", survey_yr = 2018) {
+  # test species: POP
+  #survey_area is called region in RACEBASE
+  sp_catch <- catch %>%
+    filter(species_code == speciescode)
 
-dat <- haul %>%
-  left_join(cruisedat, 
-            by = c("cruisejoin", "region")) %>%
-  filter(abundance_haul == "Y" &
-    region == survey_area) %>%
-  left_join(sp_catch, by = "hauljoin") %>%
-  replace_na(list(weight = 0, 
-                  number_fish = 0)) %>%
-  select(
-    cruisejoin.x, vessel.x, haul.x,
-    haul_type, performance, duration,
-    stratum,
-    distance_fished, weight, year,
-    weight, number_fish,
-    start_latitude, start_longitude,
-    gear_temperature, surface_temperature,
-    AreaSwept_km2
-  ) %>%
-  rename(
-    Lat = start_latitude,
-    Lon = start_longitude,
-    Year = year,
-    Catch_KG = weight,
-    Vessel = vessel.x,
-    Bottom_temp = gear_temperature,
-    Surface_temp = surface_temperature
-  ) %>%
-  filter(Year == survey_yr)
+  dat <- haul %>%
+    left_join(cruisedat,
+      by = c("cruisejoin", "region")
+    ) %>%
+    filter(abundance_haul == "Y" &
+      region == survey_area) %>%
+    left_join(sp_catch, by = "hauljoin") %>%
+    replace_na(list(
+      weight = 0,
+      number_fish = 0
+    )) %>%
+    select(
+      cruisejoin.x, vessel.x, haul.x,
+      haul_type, performance, duration,
+      stratum,
+      distance_fished, weight, year,
+      weight, number_fish,
+      start_latitude, start_longitude,
+      gear_temperature, surface_temperature,
+      AreaSwept_km2
+    ) %>%
+    rename(
+      Lat = start_latitude,
+      Lon = start_longitude,
+      Year = year,
+      Catch_KG = weight,
+      Vessel = vessel.x,
+      Bottom_temp = gear_temperature,
+      Surface_temp = surface_temperature
+    ) %>%
+    filter(Year == survey_yr)
 
-x <- dat %>%
-  mutate(wCPUE = Catch_KG/AreaSwept_km2,
-         nCPUE = number_fish/AreaSwept_km2) 
+  x <- dat %>%
+    mutate(
+      wCPUE = Catch_KG / AreaSwept_km2,
+      nCPUE = number_fish / AreaSwept_km2
+    )
+
+  return(x)
+}
+
+# POP
+get_haul_cpue()
+
+# walleye pollock
+tail(get_haul_cpue(speciescode = 21740))
+
+# Pacific halibut
+head(get_haul_cpue(speciescode = 10120))
+
+
 
 x2 <- x %>%
   group_by(Year, stratum) %>%
-  summarize(meanwCPUE = mean(wCPUE), 
-            sumvarwCPUE = sum(var(wCPUE)),
-            meannCPUE = mean(nCPUE),
-            sumvarnCPUE = sum(var(nCPUE))
-            ) %>%
+  summarize(
+    meanwCPUE = mean(wCPUE),
+    sumvarwCPUE = sum(var(wCPUE)),
+    meannCPUE = mean(nCPUE),
+    sumvarnCPUE = sum(var(nCPUE))
+  ) %>%
   ungroup()
 
-x3 <- x2 %>% 
+x3 <- x2 %>%
   left_join(ai_strata)
 
 # Total survey area
-At <- sum(ai_strata$area) 
+At <- sum(ai_strata$area)
 
 # Total CPUE for species and year across AI
 x4 <- x3 %>%
   group_by(Year, stratum) %>%
-  summarize(wCPUE = sum(meanwCPUE*area),
-            nCPUE = sum(meannCPUE*area)) %>%
+  summarize(
+    wCPUE = sum(meanwCPUE * area),
+    nCPUE = sum(meannCPUE * area)
+  ) %>%
   ungroup() %>%
   group_by(Year) %>%
-  summarize(wCPUE_total = sum(wCPUE)/At,
-            nCPUE_total = sum(nCPUE)/At)
+  summarize(
+    wCPUE_total = sum(wCPUE) / At,
+    nCPUE_total = sum(nCPUE) / At
+  )
