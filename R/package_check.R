5ba02a94f4b16d931775c77522029504a9a2a32a
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2021-06-14 21:42:31 -0700

    add R subdir and package_check fn

---
+++ R/package_check.R
@@ -0,0 +1,12 @@
+# package_check function (by Vikram Baliga)
+
+## Now load or install&load all
+package_check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
