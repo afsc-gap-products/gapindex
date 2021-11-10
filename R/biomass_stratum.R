055002d6ecc566aa56caf355ac6680ee260a2543
mcsiple-NOAA <margaret.siple@noaa.gov>
2021-11-09 16:55:52 -0800

    move data retrieval to another script

--- R/biomass_stratum.R
+++ R/biomass_stratum.R
@@ -10,71 +10,9 @@
 # Stratum areas to use
 # SQL Developer: AFSC--> Other Users --> GOA --> Tables --> GOA_STRATA
 
-# Step 1: Get RACEBASE data -----------------------------------------------
-# Where to put the cleaned/wrangled ---------------------------------------
-#data_destination <- "C:/Users/margaret.siple/Work/design-based-indices/data"
-
-library(tidyverse)
-
-# Load Oracle data (takes a while) ----------------------------------------
-# This local folder contains csv files of all the RACEBASE tables.
-a <- list.files(
-  path = here::here("..", "Oracle connection", "data", "oracle", "racebase"),
-  pattern = "\\.csv"
-)
-
-for (i in 1:length(a)) {
-  b <- read.csv(file = paste0(here::here(
-    "..", "Oracle connection",
-    "data", "oracle", "racebase", a[i]
-  )))
-  b <- janitor::clean_names(b)
-  if (names(b)[1] %in% "x1") {
-    b$x1 <- NULL
-  }
-  assign(x = gsub(pattern = "\\.csv", replacement = "", x = a[i]), value = b)
-}
-
-
-# Get stratum areas from GOA_STRATA table----------------------------------
-goa_ai_strata <- read.csv(here::here(
-  "..", "Oracle connection", "data",
-  "oracle", "goa", "goa_strata.csv"
-))
-
-goa_strata <- goa_ai_strata %>%
-  janitor::clean_names() %>%
-  filter(survey == "GOA")
-
-ai_strata <- goa_ai_strata %>%
-  janitor::clean_names() %>%
-  filter(survey == "AI")
-
-# Add area swept to each haul ---------------------------------------------
-head(haul)
-nrow(haul)
-haul <- haul %>%
-  mutate(AreaSwept_km2 = distance_fished * (0.001 * net_width)) # bc distance in km and width in m
-# This should match the EFFORT column in AI/CPUE
-
-# Get cruise info from RACE.DATA ------------------------------------------
-# AFAIK you can only get the name of the survey from the cruises.csv file, which is from RACEDATA
-cruises <- read.csv(here::here(
-  "..", "Oracle connection",
-  "data", "oracle",
-  "race_data",
-  "cruises.csv"
-)) %>%
-  janitor::clean_names()
-
-cruisedat <- cruises %>%
-  dplyr::select(year, survey_name, region, 
-         cruisejoin, cruise_data_in_racebase, 
-         vessel_country, survey_definition_id)
-
 
 # This function replicates the tables in RACEBASE GOA --> CPUE
-get_haul_cpue <- function(racebase_tables = list(cruisedat = cruisedat, 
+get_cpue <- function(racebase_tables = list(cruisedat = cruisedat, 
                                                  haul = haul, 
                                                  catch = catch), 
                           speciescode = 30060, #POP
@@ -141,7 +79,7 @@ get_haul_cpue <- function(racebase_tables = list(cruisedat = cruisedat,
 # sablefish: 20510
 
 # RACEBASE equivalent table: CPUE
-x <- get_haul_cpue(survey_yr = 2021, survey_area = "GOA", speciescode = 30060)
+x <- get_cpue(survey_yr = 2021, survey_area = "GOA", speciescode = 30060)
 
 
 # Total survey area
