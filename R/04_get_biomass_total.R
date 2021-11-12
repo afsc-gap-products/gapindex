ab3ebfbdcfd6b7cc5990529b66d72f2173265044
mcsiple-NOAA <margaret.siple@noaa.gov>
2021-11-12 13:08:39 -0800

    format biomass_total section, turn into function

--- R/04_get_biomass_total.R
+++ R/04_get_biomass_total.R
@@ -3,26 +3,36 @@
 # RACEBASE equivalent table: BIOMASS_TOTAL
 # BIOMASS_TOTAL has SURVEY_AREA, YEAR, SPECIES CODE, HAUL_COUNT, CATCH_COUNT, MEAN_WGT_CPUE, VAR_WGT_CPUE, MEAN_NUM_CPUE, VAR_NUM_CPUE, TOTAL_BIOMASS, BIOMASS_VAR, MIN_BIOMASS, MAX_BIOMASS, TOTAL_POP, POP_VAR, MIN_POP, MAX_POP
 
-biomass_total <- biomass_stratum %>%
-  dplyr::group_by(year) %>%
-  dplyr::summarize(
-    haul_count = sum(haul_count),
-    catch_count = sum(catch_count),
-    mean_wgt_cpue = sum(mean_wgt_cpue * area, na.rm = TRUE) / At, # weighted avg cpue across stata * total area
-    mean_num_cpue = sum(mean_num_cpue * area, na.rm = TRUE) / At, # could this be weighted a different way??
-    var_wgt_cpue = sum(stratum_ratio^2 * var_wgt_cpue, na.rm = TRUE),
-    var_num_cpue = sum(stratum_ratio^2 * var_num_cpue, na.rm = TRUE),
-    total_biomass = sum(stratum_biomass), # checked - ok
-    biomass_var = sum(biomass_var), # checked - ok
-    # min_biomass = ,
-    # max_biomass = ,
-    total_pop = sum(stratum_pop), # checked
-    pop_var = sum(pop_var) # checked
-    # min_pop = ,
-    # max_pop =
-    #
-    # same for total_population
-  )
+get_biomass_total <- function(racebase_tables = list(
+                                cruisedat = cruisedat,
+                                haul = haul,
+                                catch = catch
+                              ),
+                              speciescode = 30060, # POP
+                              survey_area = "AI",
+                              vulnerability = 1) {
+  biomass_stratum <- get_biomass_stratum(racebase_tables = racebase_tables, speciescode = speciescode, survey_area = survey_area, vulnerability = vulnerability)
+  biomass_total <- biomass_stratum %>%
+    dplyr::group_by(year) %>%
+    dplyr::summarize(
+      haul_count = sum(haul_count),
+      catch_count = sum(catch_count),
+      mean_wgt_cpue = sum(mean_wgt_cpue * area, na.rm = TRUE) / At, # weighted avg cpue across stata * total area
+      mean_num_cpue = sum(mean_num_cpue * area, na.rm = TRUE) / At,
+      var_wgt_cpue = sum(stratum_ratio^2 * var_wgt_cpue, na.rm = TRUE),
+      var_num_cpue = sum(stratum_ratio^2 * var_num_cpue, na.rm = TRUE),
+      total_biomass = sum(stratum_biomass), # checked - ok
+      biomass_var = sum(biomass_var), # checked - ok
+      # min_biomass = ,
+      # max_biomass = ,
+      total_pop = sum(stratum_pop), # checked
+      pop_var = sum(pop_var) # checked
+      # min_pop = ,
+      # max_pop =
+      #
+      # same for total_population
+    )
 
 
-biomass_total
+  return(biomass_total)
+}
