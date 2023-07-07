84c0601c170bef36fc6d2b23ab82ee9bcc0ad034
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-07-07 13:45:20 -0700

    first init

---
+++ R/calc_ALK.R
@@ -0,0 +1,198 @@
+#' Calculate age-length key (alk)
+#' 
+#' @description 
+#'            
+#' @param racebase_tables data object created from `gapindex::get_data()`.
+#' @param unsex character option to determine how unsexed individuals are 
+#' treated. Option "all" means that unsexed alks are determined by combining 
+#' all sexes (males, females, and unsexed) and is the option used for standard
+#' design-based composition production. Option "unsex" means that unsexed alk 
+#' are only determined by unsexed individuals are is the option used for creating
+#' data inputs for model-based age composition indices. Defaults to "unsex"
+#' @param global boolean. Should missing length bins be filled by using an 
+#' alk consisting of all years? Defaults to TRUE. 
+#' 
+#' @return dataframe of probabilities ("age_frac") of ages ("AGE") by length 
+#' (LENGTH_MM) for a given survey ("SURVEY"), year ("YEAR"), species 
+#' (SPECIES_CODE), and sex (SEX). 
+#' 
+
+calc_ALK <- function(racebase_tables = NULL,
+                     unsex = c("all", "unsex")[2], 
+                     global = TRUE) {
+  
+  ## Error Check
+  if (is.null(x = racebase_tables$size)) 
+    stop("racebase_tables$size must not be NULL. Either the taxon does not 
+         have size information or rerun gapindex::get_data() with 
+         argument pull_lengths = TRUE")
+  
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##   Extract haul, cruise and specimen data from `racebase_tables`
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  haul <- racebase_tables$haul
+  cruise <- racebase_tables$cruise
+  size <- racebase_tables$size
+  
+  ## Remove unaged otoliths (records with NA ages) from `specimen` df
+  specimen <- subset(x = racebase_tables$specimen,
+                     subset = !is.na(x = AGE) )
+  
+  ## Make sure lengths are rounded to the nearest 10 mm. Fish are measured to 
+  ## the nearest cm but crab are measured to the nearest mm. 
+  specimen$LENGTH <- round(x = specimen$LENGTH / 10)* 10
+  
+  ## Merge "SURVEY" column from the cruise information to `specimen` and `size`
+  ## using "CRUISEJOIN" as the key.
+  specimen <- merge(x = specimen,
+                    y = cruise[, c("CRUISEJOIN", "SURVEY", "YEAR")],
+                    by = "CRUISEJOIN")
+  size <- merge(x = size,
+                y = cruise[, c("CRUISEJOIN", "SURVEY", "YEAR")],
+                by = "CRUISEJOIN")
+  
+  ## Merge "YEAR" and "STRATUM" columns from `haul` to `specimen`, using
+  ## "HAULJOIN" as a key. all.x = TRUE so that specimen from non-standard hauls
+  ## are not included in the standard haul argument. These non-standard hauls
+  ## will not have YEAR information, pull the "YEAR" from the cruise value.
+  specimen <- merge(x = specimen, 
+                    y = haul[, c("HAULJOIN", "STRATUM", "STATIONID")],
+                    by = "HAULJOIN",
+                    all.x = T)                  
+  
+  specimen$YEAR <- ifelse(test = is.na(specimen$YEAR), 
+                          no = specimen$YEAR,
+                          yes = as.numeric(substr(x = specimen$CRUISE, 
+                                                  start = 1, stop = 4)))
+  
+  ## Remove specimen without stratum information
+  specimen <- subset(x = specimen, 
+                     subset = !is.na(STRATUM))
+  
+  ## Renamve "LENGTH" column add a "FREQ" column of 1s for tabulations
+  names(specimen)[names(specimen) == "LENGTH"] <- "LENGTH_MM"
+  specimen$FREQ <- 1
+  
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##   Aggregate specimen information: s_yklm will be the total number 
+  ##   of age-specimens collected in year-y, species-k, length-l, sex-m. 
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  s_yklm <- stats::aggregate(
+    FREQ ~ SURVEY + YEAR + SPECIES_CODE + SEX + LENGTH_MM + AGE,
+    data = specimen,
+    FUN = sum)
+  
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##   Calculate distribution of age proportions for a given length, `p_yklm`.
+  ##   This is the non-global age-length key.
+  ##   
+  ##   Split `s_yklm` by survey, year, species, sex, and length 
+  ##   and then calculate the proportion of distribution of observed ages 
+  ##   within each split (`p_yklm`, proportion of age for year-y, species-k, 
+  ##   length-l, and sex-m). 
+  ##   Then, rbind the splitted lists back together into one df.
+  ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  p_yklm <- 
+    do.call(what = rbind, 
+            args = lapply(X = split(x = s_yklm, 
+                                    f = list(s_yklm$SURVEY,
+                                             s_yklm$YEAR, 
+                                             s_yklm$SPECIES_CODE, 
+                                             s_yklm$SEX,
+                                             s_yklm$LENGTH_MM)),
+                          FUN = function(df) {
+                            
+                            df$AGE_FRAC <- df$FREQ / sum(df$FREQ)
+                            
+                            return(subset(x = df, select = -FREQ))
+                          }))
+  
+  ## To figure out which length-bins we need to fill in, we tabulate
+  ## every length bin encountered in the length subsample (`size`) for a
+  ## given survey, year, species, and sex. 
+  every_combo_of_lengths <- 
+    expand.grid(SURVEY = sort(unique(size$SURVEY)),
+                YEAR = sort(unique(size$YEAR)),
+                SPECIES_CODE = sort(unique(size$SPECIES_CODE)),
+                SEX = sort(unique(size$SEX)),
+                LENGTH_MM = seq(from = min(size$LENGTH, na.rm = TRUE),
+                                to = max(size$LENGTH, na.rm = TRUE),
+                                by = 10),
+                AGE = seq(from = min(specimen$AGE, na.rm = TRUE),
+                          to = max(specimen$AGE, na.rm = TRUE),
+                          by = 1))
+  
+  p_yklm <- merge(x = every_combo_of_lengths,
+                  y = p_yklm,
+                  all.x = TRUE,
+                  by = c("SURVEY", "YEAR", "SPECIES_CODE", 
+                         "SEX", "LENGTH_MM", "AGE"))
+  
+  if (global) {
+    
+    missing_lengths <- 
+      do.call(what = rbind, 
+              args = lapply(X = split(x = p_yklm, 
+                                      f = with(p_yklm, list(SURVEY,
+                                                            YEAR, 
+                                                            SPECIES_CODE, 
+                                                            SEX,
+                                                            LENGTH_MM))),
+                            FUN = function(df) {
+                              if (sum(df$AGE_FRAC, na.rm = TRUE) == 0)
+                                return(data.frame(
+                                  SURVEY = unique(df$SURVEY),
+                                  YEAR =  unique(df$YEAR),
+                                  SPECIES_CODE = unique(df$SPECIES_CODE),
+                                  SEX = unique(df$SEX),
+                                  LENGTH_MM = unique(df$LENGTH_MM))) 
+                            }
+              )
+      )
+    
+    ## Aggregate specimen information over years to calculate a global ALK,
+    ## i.e., an ALK that is aggregated over years so we can fill in years
+    ## where a length-bin for a sex was not encountered.
+    s_klm <- stats::aggregate(
+      FREQ ~ SURVEY + SPECIES_CODE + SEX + LENGTH_MM + AGE,
+      data = specimen,
+      FUN = sum)
+    
+    p_klm <- 
+      do.call(what = rbind, 
+              args = lapply(X = split(x = s_klm, 
+                                      f = list(s_klm$SURVEY,
+                                               s_klm$SPECIES_CODE, 
+                                               s_klm$SEX,
+                                               s_klm$LENGTH_MM)),
+                            FUN = function(df) {
+                              df$AGE_FRAC <- df$FREQ / sum(df$FREQ)
+                              return(df)
+                            }))
+    
+    ## For each record in `missing_lengths`, merge all the age probabilities
+    ## from `p_klm` for that length bin. 
+    filled_in_lengths <- 
+      merge(x = missing_lengths,
+            y = p_klm,
+            all.x = TRUE,
+            by = c("SURVEY", "SPECIES_CODE", "SEX", "LENGTH_MM"))
+    
+    ## Append the globally-filled lengths with the the non-global `p_yklm` alk
+    ## to get a now global alk. 
+    p_yklm <- rbind(subset(x = p_yklm, subset = !is.na(AGE_FRAC)),
+                    subset(x = filled_in_lengths, 
+                           subset = !is.na(AGE_FRAC),
+                           select = names(p_yklm)))
+    
+    p_yklm <- merge(x = every_combo_of_lengths,
+                    y = p_yklm,
+                    all.x = TRUE,
+                    by = c("SURVEY", "YEAR", "SPECIES_CODE", 
+                           "SEX", "LENGTH_MM", "AGE"))
+  }
+  
+  p_yklm$AGE_FRAC[is.na(p_yklm$AGE_FRAC)] <- 0
+  return(p_yklm)
+}
+
---
+++ R/calc_sizecomp_haul.R
@@ -0,0 +1,86 @@
+#' Calculate numerical CPUE at length by haul
+#' 
+#' @description Uses Equation 16 in Wakabayashi et al. 1985 to calculate
+#'              numbers-catch-per-unit-effort (CPUE, Numbers/km2) at length.
+#'            
+#' @param racebase_tables data object created from `gapindex::get_data()`.
+#' @param racebase_cpue object created from `gapindex::calc_cpue()`.
+#' 
+#' @return dataframe of numerical CPUE ("S_ijklm_NO_KM2") by survey ("SURVEY"), 
+#' year ("YEAR"), stratum ("STRATUM"), haul ("HAULJOIN), species (SPECIES_CODE), 
+#' sex (SEX), and length (LENGTH_MM). 
+#' 
+
+calc_sizecomp_haul <- function(racebase_tables = NULL,
+                               racebase_cpue = NULL) {
+  
+  ## Error Check
+  if (is.null(x = racebase_tables$size)) 
+    stop("racebase_tables$size must not be NULL. Either the taxon does not 
+         have size information or rerun gapindex::get_data() with 
+         argument pull_lengths = TRUE")
+  
+  ## Subset dataframe haul based on the CRUISEJOIN field in dataframe cruise. 
+  ## Attach SURVEY and YEAR information to dataframe haul using CRUISJOIN
+  ## as the key. 
+  cruise <- racebase_tables$cruise
+  haul <- subset(x = racebase_tables$haul,
+                 subset = CRUISEJOIN %in% cruise$CRUISEJOIN)
+  haul <- merge(x = haul, 
+                y = cruise[, c("CRUISEJOIN", "SURVEY", "YEAR")],
+                by = "CRUISEJOIN")
+  
+  cpue <- subset(x = racebase_cpue,
+                 subset = CRUISEJOIN %in% cruise$CRUISEJOIN)
+  size <- subset(x = racebase_tables$size, 
+                 subset = HAULJOIN %in% haul$HAULJOIN)
+  
+  ## Attach YEAR, SURVEY,  and STRATUM information from dataframe `haul to 
+  ## dataframe `size` using HAULJOIN as the key.
+  size <-  merge(x = size[, c("HAULJOIN", "SPECIES_CODE",
+                              "LENGTH", "FREQUENCY", "SEX")],
+                 y = cpue[, c("SURVEY", "HAULJOIN", "YEAR",
+                              "SPECIES_CODE", "CPUE_NOKM2")],
+                 by = c("HAULJOIN", "SPECIES_CODE"))
+  
+  
+  
+  ##############################################
+  ## Wakabayashi et al. 1985 Equation 16: 
+  ##############################################
+  ## At each station with length-frequency records, the number of individuals
+  ##     within each sex/length class was estimated by expanding the 
+  ##     length-frequency subsample to the total catch (per area swept). 
+  ##     This is S_ijklm, the estimated number of individuals of species-k 
+  ##     with sex-m and length-l caught at station-j within stratum-i.
+  ##
+  ## s_ijklm is the FREQUENCY column of the size df, the number of records
+  ##     of sex-m and length-l of species-k recorded at station-j in stratum-i
+  ## s_ijk is the frequency of recorded individuals summed over sex and length
+  ##        class, i.e., the number of individuals of species-k recorded 
+  ##        at station-j in stratum-i summed over all length and sex classes
+  ## S_ijk is the estimated numbers per area swept from the cpue df
+  ##     of species-k at station-j in stratum-i. The notation S_ijk is not 
+  ##     used below, instead CPUE_NOKM2 is used. 
+  ##
+  ## s_ijk and NUMCPUE_IND_KM2 are merged into the size df by joining 
+  ##     via the HAULJOIN value and species code so that calculations can
+  ##     be vectorized easily.
+  
+  s_ijk <- stats::aggregate(
+    FREQUENCY ~ SURVEY + YEAR + HAULJOIN + SPECIES_CODE,
+    data = size,
+    FUN = sum)
+  names(s_ijk)[names(s_ijk) == "FREQUENCY"] <- "s_ijk"
+  
+  size <- merge(x = size,
+                y = s_ijk[, c("SPECIES_CODE", "HAULJOIN", "s_ijk")],
+                by = c("SPECIES_CODE", "HAULJOIN"))
+  
+  size$S_ijklm_NOKM2 <- size$FREQUENCY / size$s_ijk * size$CPUE_NOKM2
+  
+  ## Add units to length and return
+  names(size)[names(size) == "LENGTH"] <- "LENGTH_MM"
+  return(size)
+  
+}
