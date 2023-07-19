a41de11a0eed6e5a6f8b3f96d0de876012e44a96
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-07-19 10:40:16 -0700

    added functionality to include strings in the query

--- R/stitch_entries.R
+++ R/stitch_entries.R
@@ -6,5 +6,9 @@
 #' @export
 #' 
 stitch_entries <- function(stitch_what = "") {
-  return( paste0("(", paste0(stitch_what, collapse=", "), ")")  )
+  
+  if (class(stitch_what) == "character") {
+    return( paste0("(", paste0(sQuote(x = stitch_what, q = F), collapse=", "), ")")  )
+  } else
+    return( paste0("(", paste0(stitch_what, collapse=", "), ")")  )
 }
