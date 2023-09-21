ae06ebe8f515a05cb77d4857758e7120c7bde43c
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-09-20 17:22:47 -0700

    added as suggestions from devtools::check

---
+++ R/gapindex-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats aggregate
+#' @importFrom stats weighted.mean
+## usethis namespace: end
+NULL
---
+++ R/globals.R
@@ -0,0 +1,4 @@
+utils::globalVariables(
+  c(read.csv("inst/extdata/metadata.csv")$Field,
+    "AGE_FRAC", "FREQ", "Field", "GROUP", 
+    "NUMBER", "S_ijklm_NOKM2", "TYPE", "colname"))
