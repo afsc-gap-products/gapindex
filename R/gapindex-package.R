48752f775351481e590cfad88d4072d06d28b001
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-03-12 01:10:55 -0800

    added imports for data.table and lifecyle deprecated

--- R/gapindex-package.R
+++ R/gapindex-package.R
@@ -2,6 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import data.table
+#' @importFrom lifecycle deprecated
 #' @importFrom stats aggregate
 #' @importFrom stats weighted.mean
 ## usethis namespace: end
