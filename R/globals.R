41f94ebcad3ef731e55c663c309d12f879fe3c27
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-03-13 11:22:27 -0700

    adding more field names

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,5 @@
 utils::globalVariables(
   c(read.csv("inst/extdata/metadata.csv")$Field,
-    "AGE_FRAC", "FREQ", "Field", "GROUP", 
-    "NUMBER", "S_ijklm_NOKM2", "TYPE", "colname"))
+    "AGE_FRAC", "FREQ", "Field", 
+    "AGEPOP", "GROUP_CODE", "NUM_AGES", "S_ijklm", "TOT_AREA", "p_ijklm",
+    "NUMBER", "S_ijklm_NOKM2", "TYPE", "colname", "."))
