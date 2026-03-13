563cd38739c70ded811f85b657bc2a813ea9ac8a
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-03-13 11:21:47 -0700

    converted into an internal equation

--- R/remove_temp_tables.R
+++ R/remove_temp_tables.R
@@ -2,10 +2,16 @@
 #' 
 #' @param channel connection to Oracle created via gapindex::get_connected() 
 #'
-#' @export
+#' @keywords internal 
 #' 
 
-remove_temp_tables <- function(channel) {
+remove_temp_tables <- function(channel = NULL) {
+  
+  if (!(class(x = channel) %in% c("RODBC", "Oracle")) | 
+      is.null(x = channel)) {
+    stop("Argument `channel` must be either an RODBC or DBI/ODBC database connection, e.g., via gapindex::get_connected()")
+  }
+  conn_type <- class(x = channel)
   
   ## Loop through temporary table names -- start
   for (itable in c("AVAIL_SPP", "CATCH", "CRUISE", "HAUL", "INPUT_SPP", 
@@ -13,17 +19,16 @@ remove_temp_tables <- function(channel) {
                    "SURVEY_DESIGN", "SURVEY", "SUBAREA", 
                    "USER_TAXONOMIC_INFO", "UNAVAIL_SPP", "USER_INPUT_SPP")) { 
     
-    ## If itable exists
-    if (nrow(x = RODBC::sqlQuery(
+    table_exists <- gapindex::sql_query(
       channel = channel,
-      query = paste0("SELECT table_name
-                      FROM user_tables
-                      WHERE TABLE_NAME = 'GAPINDEX_TEMPORARY_",  
-                     itable, "_QUERY'"))) != 0) {
-      ## Drop the table
-      RODBC::sqlQuery(channel = channel, 
-                      query = paste0("DROP TABLE ", "GAPINDEX_TEMPORARY_", 
-                                     itable, "_QUERY PURGE;"))
-    }
+      query = paste0("SELECT table_name FROM user_tables WHERE TABLE_NAME = ",
+                    "'GAPINDEX_TEMPORARY_", itable, "_QUERY'")
+    ) |> nrow() != 0
+    
+    ## Drop and purge the temporary table if it exists
+    if (table_exists) 
+      gapindex::sql_query(channel = channel, 
+                          query = paste0("DROP TABLE ", "GAPINDEX_TEMPORARY_", 
+                                         itable, "_QUERY PURGE;"))
   } ## Loop through temporary table names -- end
 }
