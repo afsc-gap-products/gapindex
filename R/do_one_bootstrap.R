5257cc7f97b681920c050d71230625ac8324a01c
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-04-29 16:29:00 -0700

    add bootstrap ids to all outputs from bootstrap function

--- R/do_one_bootstrap.R
+++ R/do_one_bootstrap.R
@@ -8,8 +8,6 @@
 #' This function bootstraps CPUE within stratum (i.e., the bootstrapped dataset will have the same number of hauls per stratum) with replacement. This is the same method used in Monnahan et al. (2026).
 #' This function returns one single resample of a CPUE table. Usually, you would be running this function several times (1 for each bootstrap you want to do of the data).
 #'
-#' @export
-#'
 #' @examples
 #' testdata <- gapindex::get_data(
 #'   year_set = c(1996, 1999),
@@ -17,7 +15,7 @@
 #'   spp_codes = c(21740, 30060, 10110)[1],
 #'   haul_type = 3,
 #'   abundance_haul = c("Y", "N")[1],
-#'   pull_lengths = FALSE
+#'   pull_lengths = TRUE
 #' )
 #' testcpue <- gapindex::calc_cpue(gapdata = testdata)
 #'
@@ -29,7 +27,10 @@
 #'   geom_jitter(width = 0.05, height = 0.00001) +
 #'   geom_jitter(data = x1$cpue, width = 0.05, color = "blue", alpha = 0.2) +
 #'   geom_jitter(data = x2$cpue, width = 0.05, color = "red", alpha = 0.2)
-do_one_bootstrap <- function(boot, gapdata, gapcpue) {
+#'
+#' @export
+#'
+do_one_bootstrap <- function(boot, gapdata = NULL, gapcpue = NULL) {
   if (length(unique(gapcpue$SPECIES_CODE)) > 1) {
     stop("More than one species detected in gapdata object. Please filter your gapdata to a single species and try again. More than one year is ok.")
   }
@@ -65,23 +66,29 @@ do_one_bootstrap <- function(boot, gapdata, gapcpue) {
     # return sampled rows
     out <- sampling::getdata(data = df_year, m = x)
     out <- out[, !(names(out) %in% c("ID_unit", "Prob", "Stratum"))]
-    #out$boot <- boot # track bootstrap #
+    # out$boot <- boot # track bootstrap #
 
     return(out)
   })
 
   # combine years
   result <- do.call(rbind, boot_list)
-  result <- as.data.table(result)
+  result <- data.table::as.data.table(result)
 
   # calculate stratum biomasses
   biomass_stratum <- gapindex::calc_biomass_stratum(gapdata = gapdata, cpue = result)
   biomass_subarea <- gapindex::calc_biomass_subarea(gapdata = gapdata, biomass_stratum = biomass_stratum)
-
+  biomass_total <- biomass_subarea[which(biomass_subarea$AREA_ID == 99903), ]
+  
+  # tag all the biomasses with their bootstrap ID so you can keep track of them
+  biomass_stratum$boot <- boot
+  biomass_subarea$boot <- boot
+  biomass_total$boot <- boot
+  
   return(list(
     "cpue" = result,
     "biomass_stratum" = biomass_stratum,
     "biomass_subarea" = biomass_subarea,
-    "boot" = boot
+    "biomass_total" = biomass_total
   ))
 }
