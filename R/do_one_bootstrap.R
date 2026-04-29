1dead5a15e1b865ea9196207cba0620f5ebd2e41
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-04-29 13:32:04 -0700

    andd roxygen skeleton, example, and warnings at the start for data structure

--- R/do_one_bootstrap.R
+++ R/do_one_bootstrap.R
@@ -1,11 +1,48 @@
-do_one_bootstrap <- function(boot, gapdata) {
-  i <- boot
-  set.seed(i)
-  
-  cpue_base <- gapindex::calc_cpue(gapdata = gapdata)
+#' Bootstrap a CPUE table from gapindex
+#'
+#' @param boot (numeric) bootstrap number.
+#' @param gapcpue object created from gapindex::calc_cpue()
+#'
+#' @returns a dataframe with one row per bootstrap
+#' @export
+#'
+#' @examples
+#' testdata <- gapindex::get_data(
+#'   year_set = c(1996, 1999),
+#'   survey_set = c("GOA", "AI", "EBS", "NBS", "BSS")[1],
+#'   spp_codes = c(21740, 30060, 10110)[1],
+#'   haul_type = 3,
+#'   abundance_haul = c("Y", "N")[1],
+#'   pull_lengths = FALSE
+#' )
+#' testcpue <- gapindex::calc_cpue(gapdata = testdata)
+#'
+#' x1 <- do_one_bootstrap(boot = 1, gapcpue = testcpue)
+#' x2 <- do_one_bootstrap(boot = 2, gapcpue = testcpue)
+#'
+#' library(ggplot2)
+#' ggplot(testcpue, aes(x = factor(YEAR), y = CPUE_KGKM2)) +
+#'   geom_jitter(width = 0.05, height = 0.00001) +
+#'   geom_jitter(data = x1, width = 0.05, color = "blue", alpha = 0.2) +
+#'   geom_jitter(data = x2, width = 0.05, color = "red", alpha = 0.2)
+do_one_bootstrap <- function(boot, gapcpue) {
+  if (length(unique(gapcpue$SPECIES_CODE)) > 1) {
+    stop("More than one species detected in gapdata object. Please filter your gapdata to a single species and try again. More than one year is ok.")
+  }
+
+  if (length(unique(gapcpue$DESIGN_YEAR)) > 1) {
+    stop("More than one survey design detected in gapdata object. This may cause issues with your resampling. Suggest limiting to one survey design.")
+  }
+
+  if (length(unique(gapcpue$SURVEY)) > 1) {
+    stop("More than one region detected in gapdata object. Filter your gapdata object to a single region and run again.")
+  }
+
+  # ensures each sample will be random but repeatable
+  set.seed(boot)
 
   # split data by year
-  yearly_data <- split(cpue_base, cpue_base[["YEAR"]])
+  yearly_data <- split(gapcpue, gapcpue[["YEAR"]])
 
   boot_list <- lapply(yearly_data, function(df_year) {
     # sample sizes within year
@@ -23,7 +60,9 @@ do_one_bootstrap <- function(boot, gapdata) {
 
     # return sampled rows
     out <- sampling::getdata(data = df_year, m = x)
-    out <- out[, !(names(out) %in% c("ID_unit","Prob", "Stratum"))]
+    out <- out[, !(names(out) %in% c("ID_unit", "Prob", "Stratum"))]
+    out$boot <- boot # track bootstrap #
+
     return(out)
   })
 
