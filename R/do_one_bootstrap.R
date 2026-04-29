c12ffb33ab3514cd220d8a0decdfbe29554ac7a1
MargaretSiple-NOAA <margaret.siple@noaa.gov>
2026-04-29 15:08:13 -0700

    style edits to bootstrapping fn

--- R/do_one_bootstrap.R
+++ R/do_one_bootstrap.R
@@ -1,12 +1,12 @@
 #' Bootstrap a CPUE table from gapindex
 #'
 #' @param boot (numeric) bootstrap number.
-#' @param gapcpue object created from gapindex::calc_cpue(). This can have multiple years of data, but should be one species and one region only. 
+#' @param gapcpue object created from gapindex::calc_cpue(). This can have multiple years of data, but should be one species and one region only.
 #'
 #' @returns a resampled CPUE table with one row per sample.
 #' @details
-#' This function returns one single resample of a CPUE table. Usually, you would be running this function several times (1 for each bootstrap you want to do of the data). 
-#' 
+#' This function returns one single resample of a CPUE table. Usually, you would be running this function several times (1 for each bootstrap you want to do of the data).
+#'
 #' @export
 #'
 #' @examples
@@ -64,7 +64,7 @@ do_one_bootstrap <- function(boot, gapdata, gapcpue) {
     # return sampled rows
     out <- sampling::getdata(data = df_year, m = x)
     out <- out[, !(names(out) %in% c("ID_unit", "Prob", "Stratum"))]
-    out$boot <- boot # track bootstrap #
+    #out$boot <- boot # track bootstrap #
 
     return(out)
   })
@@ -72,12 +72,15 @@ do_one_bootstrap <- function(boot, gapdata, gapcpue) {
   # combine years
   result <- do.call(rbind, boot_list)
   result <- as.data.table(result)
-  
+
   # calculate stratum biomasses
   biomass_stratum <- gapindex::calc_biomass_stratum(gapdata = gapdata, cpue = result)
   biomass_subarea <- gapindex::calc_biomass_subarea(gapdata = gapdata, biomass_stratum = biomass_stratum)
-  
-  return(list("cpue" = result, 
-              "biomass_stratum" = biomass_stratum,
-              "biomass_subarea" = biomass_subarea))
+
+  return(list(
+    "cpue" = result,
+    "biomass_stratum" = biomass_stratum,
+    "biomass_subarea" = biomass_subarea,
+    "boot" = boot
+  ))
 }
